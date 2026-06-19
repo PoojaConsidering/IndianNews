f8edf378ea36ec6cea8d0dc11096f337b270bb12
PoojaConsidering <kalasadakeri665@gmail.com>
2026-06-20 00:52:48 +0530

    scraper correction

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from sites import SITES
+from scraper.sites import SITES
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
